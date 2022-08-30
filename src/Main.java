01b942561a35a1143cd6affa6fd3ded4231fde34
marek <marek-schuetze@mail.de>
2022-08-30 21:02:26 +0200

    Add priint function

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         count(5,30);
+        print("");
     }
      public static void count (int start, int end) {
         for (int i = start; i <= end; i++){
             System.out.println(i);
         }
+     }
+     public static void print(String input){
+         System.out.println(input);
+
      }
 
 }
