da1ccf3433c76eeadc2e81eafc8d671079bfc487
marek <marek-schuetze@mail.de>
2022-08-30 21:01:07 +0200

    Add array function

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         count(5,30);
+        make_array(10);
+
     }
      public static void count (int start, int end) {
         for (int i = start; i <= end; i++){
             System.out.println(i);
         }
      }
-
-}
+     public static void make_array(int length){
+        for(int i = 0; i < length; i++){
+            int array[] = new int[length];
+            array[i] = i;
+            System.out.println(array[i]);
+            }
+        }
+     }
