257c80bf2e64431e8322ba03bb1c26ff63dcf088
marek <marek-schuetze@mail.de>
2022-08-30 20:39:39 +0200

    Add count function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        count(5,30);
     }
+     public static void count (int start, int end) {
+        for (int i = start; i <= end; i++){
+            System.out.println(i);
+        }
+     }
+
 }
